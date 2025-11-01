7318423b1d79282e2bfe56248d6cd3c83f7a05f4
SylkieGamer <110408156+SylkieGamer@users.noreply.github.com>
2025-11-01 19:13:49 -0400

    Updates hello.py
    Updates with a shortlist of commands I used today for reference.

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
 print("Hello Git! This is my first commit from the terminal!")
+
+Here's my short list of commands I used
+ssh -T git@github.com
+code ~/.ssh/config
+git push origin main
+git status
+
+Make a .py, opens it in the ide, save and exit, load/add the file to be pushed, commit the push with an identifying string, actually push to main 
+touch hello.py
+ls
+code hello.py
+git status
+git add hello.py
+git status
+git commit -m "I'm learning O_O"
+git status
+git push origin main
